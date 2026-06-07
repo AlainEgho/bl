208f7f3777b7fa8525168df1293ed522edecac72
Alain Egho <ing.alainegho@gmail.com>
2026-06-07 21:53:10 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.set('trust proxy', true);
   app.use(new HttpLoggerMiddleware().use.bind(new HttpLoggerMiddleware()));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors({ origin: ['http://localhost:4200','https://bl-njnyvp1er-alain-s-projects15.vercel.app','https://alainegho.github.io'], credentials: true ,
+  app.enableCors({ origin: ['*'], credentials: true ,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: '*',
 
