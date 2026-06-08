9b14d5272eef506e093d8d49009b991e39e0a5b8
AlainEgho <ing.alainegho@gmail.com>
2026-06-08 19:52:57 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   
-   app.enableCors({ origin: 'https://ux-seven-phi.vercel.app',
+   app.enableCors({ origin: 'https://bl-rosy.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
      credentials:true,
                    allowedHeaders: '*',
